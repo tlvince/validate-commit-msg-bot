4458292cd055403dde194c498549c837b1489bad
Tom Vincent <git@tlvince.com>
2017-07-02 19:40:15 +0100

    chore: fix passing token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-now="npm run now --token=$NOW_TOKEN"
+now="npm run -s now -- --token=$NOW_TOKEN"
 repo_name="${TRAVIS_REPO_SLUG##*/}"
-
-$now --public
-$now rm --safe --yes "$repo_name"
+$now --public && $now rm --safe --yes "$repo_name"
