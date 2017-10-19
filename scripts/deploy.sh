bbecee9b7280aa9490617a96c5abd1aa30d90b0e
Tom Vincent <git@tlvince.com>
2017-10-19 18:03:55 +0100

    chore(deploy): expand steps, exit if any non-zero

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -euo pipefail
+
 now="npm run -s now -- --token=$NOW_TOKEN"
 repo_name="${TRAVIS_REPO_SLUG##*/}"
-$now --public && $now alias && $now rm --safe --yes "$repo_name"
+
+$now --public
+$now alias
+$now rm --safe --yes "$repo_name"
